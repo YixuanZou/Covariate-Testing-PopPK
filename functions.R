bac4fec871b59e0f8c59d80f8369dfbac9671e26
YixuanZou <yixuanzou@gmail.com>
2020-07-14 22:46:40 -0700

    Add more simulation option
    I added the simulation option for choosing omega, sigma and the correlation between clearance and volume. Also I wrote the main function to test competing covariates.

--- functions.R
+++ functions.R
@@ -2,7 +2,7 @@
 # Title: R functions for simulation study
 # Author: Yixuan Zou
 # Create Date: 5/26/2020
-# Last Update: 6/26/2020
+# Last Update: 7/12/2020
 #-------------------------------------------------------------
 
 #-------------------------------------------------------------
@@ -51,18 +51,18 @@ create_data_template <- function(sample_design, sample_size, cov_name){
     }
   }
   data <- data.frame(ID, TIME, EVID, AMT, RATE, DV)
-  pat_data <- read.csv('pat_sim_data.csv')
+  pat_data <- read.csv('PAT_SIM_DATA.CSV')
   pat_sub_data <- sample_n(pat_data, sample_size)
   pat_sub_data$ID <- 1:sample_size
   cov_median <- median(pat_sub_data[, colnames(pat_sub_data) == cov_name])
   final_data <- merge(x = data, y = pat_sub_data, by = "ID", all.x = TRUE)
   write.table(final_data, 'FULL_SIM_DATA.CSV', sep = ',', row.names = FALSE, col.names = FALSE)
-  write.table(cov_median, 'COV_MEDIAN.txt', sep = '', row.names = FALSE, col.names = FALSE)
+  write.table(cov_median, 'COV_MEDIAN.TXT', sep = '', row.names = FALSE, col.names = FALSE)
 }
 
 # create the NONMEM script of the base model
 create_baseNM <- function(script){
-    cov_names <- paste(colnames(read.csv('pat_sim_data.csv'))[-1], collapse = " ")
+    cov_names <- paste(colnames(read.csv('PAT_SIM_DATA.CSV'))[-1], collapse = " ")
     base_model_ctl <- c("$PROBLEM 1-COMPARTMENT MODEL",
                         gsub("&", cov_names, "$INPUT ID TIME EVID AMT RATE DV &"),
                         "$DATA DATA_SIM.CSV IGNORE=@",
@@ -81,9 +81,9 @@ create_baseNM <- function(script){
                         "$THETA",
                         "(1E-10, 0.1)",
                         "(1E-10, 1)",
-                        "$OMEGA",
+                        "$OMEGA BLOCK(2)",
                         "0.1",
-                        "0.1",
+                        "0.025 0.1",
                         "$SIGMA",
                         "0.1",
                         "0 FIXED",
@@ -95,8 +95,9 @@ create_baseNM <- function(script){
 }
 
 # create the simulation NONMEM script with the covariate
-create_simCovNM <- function(coef, cov_name, cov_median, sim_index, omegasq, sigmasq){
-  cov_names <- paste(colnames(read.csv('pat_sim_data.csv'))[-1], collapse = " ")
+create_simCovNM <- function(coef, cov_name, cov_median, sim_index, 
+                            cl_v_corr, omegasq, sigmasq){
+  cov_names <- paste(colnames(read.csv('PAT_SIM_DATA.CSV'))[-1], collapse = " ")
   data_sim_ctl <- c(
     "$PROBLEM SIMULATION DATA",
     gsub("&", cov_names, "$INPUT ID TIME EVID AMT RATE DV &"),
@@ -116,9 +117,9 @@ create_simCovNM <- function(coef, cov_name, cov_median, sim_index, omegasq, sigm
     "(0.1, FIXED)",
     "(1, FIXED)",
     gsub("&", coef, "(&, FIXED)"),
-    "$OMEGA",
-    gsub("&", omegasq, "& FIXED"),
-    gsub("&", omegasq, "& FIXED"),
+    "$OMEGA BLOCK(2)",
+    gsub("&", omegasq, "&"),
+    gsub("&1", cl_v_corr^2*omegasq, gsub("&2", omegasq, "&1 &2 FIXED")),
     "$SIGMA",
     gsub("&", sigmasq, "& FIXED"),
     "0 FIXED",
@@ -138,7 +139,7 @@ create_scoreNM <- function(cov_name, cov_median, base_result_file, script){
   base_result <- read.table(base_result_file,header = T, sep = "",
                             as.is= T,skip= 1)
   estimates <- base_result[base_result$ITERATION == -1000000000, ][-c(1, ncol(base_result))]
-  cov_names <- paste(colnames(read.csv('pat_sim_data.csv'))[-1], collapse = " ")
+  cov_names <- paste(colnames(read.csv('PAT_SIM_DATA.CSV'))[-1], collapse = " ")
   score_model_ctl <- c(
     "$PROBLEM SIMULATION DATA",
     gsub("&", cov_names, "$INPUT ID TIME EVID AMT RATE DV &"),
@@ -157,9 +158,9 @@ create_scoreNM <- function(cov_name, cov_median, base_result_file, script){
     "THETA1",
     "THETA2",
     "1.0",
-    "$OMEGA",
+    "$OMEGA BLOCK(2)",
     "OMEGA.1.1.",
-    "OMEGA.2.2.",
+    "OMEGA.2.1. OMEGA.2.2.",
     "$SIGMA",
     "SIGMA.1.1.",
     "0 FIXED",
@@ -173,7 +174,7 @@ create_scoreNM <- function(cov_name, cov_median, base_result_file, script){
     score_model_ctl[startsWith(score_model_ctl, "CL=")] = line2
   }
   for(para_name in colnames(estimates)){
-    if(estimates[para_name] > 0){
+    if(estimates[para_name] != 0){
       score_model_ctl <- gsub(para_name, estimates[para_name], score_model_ctl)
     }
   }
@@ -182,7 +183,7 @@ create_scoreNM <- function(cov_name, cov_median, base_result_file, script){
 
 # create the covariate model NONMEM script for LRT and Wald's test
 create_covNM <- function(cov_name, cov_median, script){
-  cov_names <- paste(colnames(read.csv('pat_sim_data.csv'))[-1], collapse = " ")
+  cov_names <- paste(colnames(read.csv('PAT_SIM_DATA.CSV'))[-1], collapse = " ")
   cov_model_ctl <- c(
     "$PROBLEM SIMULATION DATA",
     gsub("&", cov_names, "$INPUT ID TIME EVID AMT RATE DV &"),
@@ -201,9 +202,9 @@ create_covNM <- function(cov_name, cov_median, script){
     "(0, 0.1)",
     "(0, 1)",
     "(0, 1)",
-    "$OMEGA",
+    "$OMEGA BLOCK(2)",
     "0.1",
-    "0.1",
+    "0.025 0.1",
     "$SIGMA",
     "0.1",
     "0 FIXED",
@@ -235,11 +236,12 @@ run_NM <- function(NM_script){
 
 # summarize the output from NONMEM
 run_analysis <- function(sample_size, sample_design, coef_index,
-                         cl_cov_list, cov_name, num_sim, omegasq, 
-                         sigmasq, home_dir){
-  dir_name_0 <- paste("Sim", sample_size, sample_design,
-                      cov_name, "omegasq", omegasq, "sigmasq", 
-                      sigmasq, coef_index, sep = "_")
+                         cl_cov_list, cov_name, num_sim, cl_v_corr,
+                         omegasq, sigmasq, home_dir){
+  dir_name_0 <-  paste("Sim", sample_size, sample_design,
+                       cov_name, "cl_v_corr", cl_v_corr, 
+                       "omegasq", omegasq, "sigmasq", 
+                       sigmasq, coef_index, sep = "_")
   proj_dir_0 <- paste(home_dir, dir_name_0, sep = "/")
   setwd(proj_dir_0)
   base_result_list <- paste0("base_model_", 1:num_sim, ".ext")
@@ -271,7 +273,7 @@ run_analysis <- function(sample_size, sample_design, coef_index,
   }
   final_result <- data.frame(sample_size, sample_design, 
                              coef=cl_cov_list[coef_index],
-                             cov_name, num_sim,
+                             cov_name, num_sim, cl_v_corr,
                              omegasq, sigmasq,
                              lrt=lrt_stat_list,
                              wald=wam_stat_list,
@@ -285,23 +287,110 @@ run_analysis <- function(sample_size, sample_design, coef_index,
   write.csv(final_result, paste(dir_name_0, ".csv", sep=""), row.names = F)  
 }
 
+
+# summarize the output from NONMEM
+run_analysis_corr <- function(sample_size, sample_design, coef_index,
+                              cl_cov_list, cov_corr, num_sim, cl_v_corr, 
+                              omegasq, sigmasq, home_dir){
+  dir_name_0 <-  paste("Sim", sample_size, sample_design,
+                       paste(cov_corr, collapse = "_"), 
+                       "cl_v_corr", cl_v_corr, 
+                       "omegasq", omegasq, 
+                       "sigmasq", sigmasq, 
+                       coef_index, sep = "_")
+  proj_dir_0 <- paste(home_dir, dir_name_0, sep = "/")
+  setwd(proj_dir_0)
+  base_result_list <- paste0("base_model_", 1:num_sim, ".ext")
+  cov_result_list <- paste0("cov_model_", 1:num_sim, ".ext")
+  score_result_list <- paste0("score_model_", 1:num_sim, ".ext")
+  score_coi_list <- paste0("score_model_", 1:num_sim, ".coi")
+  score_stat_list <- wam_stat_list <- lrt_stat_list <- c()
+  cov_result_corr_list <- paste0("cov_model_corr", 1:num_sim, ".ext")
+  score_result_corr_list <- paste0("score_model_corr", 1:num_sim, ".ext")
+  score_coi_corr_list <- paste0("score_model_corr", 1:num_sim, ".coi")
+  score_stat_corr_list <- wam_stat_corr_list <- lrt_stat_corr_list <- c()
+  for (i in 1:num_sim) {
+    tryCatch({
+      base_obj <- read_obj(base_result_list[i])
+      cov_obj <-  read_obj(cov_result_list[i])
+      # likelihood ratio statistic
+      lrt_stat_list[i] <- base_obj - cov_obj
+      # wald's test
+      cov_result <- read.table(cov_result_list[i], header = T, sep = "",
+                               as.is= T,skip= 1) 
+      estimate <- cov_result[cov_result$ITERATION == -1000000000, "THETA3"]
+      sd <- cov_result[cov_result$ITERATION == -1000000001, "THETA3"]
+      wam_stat_list[i] <- ifelse(length(sd)>0, (estimate/sd)^2, 0)
+      # score's test 
+      score_result <- read.table(score_result_list[i], header = T, sep = "",
+                                 as.is= T,skip= 1)
+      score_coi <- read.table(score_coi_list[i], header = T, sep = "",
+                              as.is= T,skip= 1)
+      score <- score_result[score_result$ITERATION == -1000000008, "THETA3"]
+      fim  <- score_coi[score_coi$NAME == "THETA3", "THETA3"]
+      score_stat_list[i] <- ifelse(length(fim)>0, score^2/fim, 0)
+      
+      cov_corr_obj <-  read_obj(cov_result_corr_list[i])
+      # likelihood ratio statistic
+      lrt_stat_corr_list[i] <- base_obj - cov_corr_obj
+      # wald's test
+      cov_corr_result <- read.table(cov_result_corr_list[i], header = T, sep = "",
+                                    as.is= T,skip= 1) 
+      estimate <- cov_corr_result[cov_corr_result$ITERATION == -1000000000, "THETA3"]
+      sd <- cov_corr_result[cov_corr_result$ITERATION == -1000000001, "THETA3"]
+      wam_stat_corr_list[i] <- ifelse(length(sd)>0, (estimate/sd)^2, 0)
+      # score's test 
+      score_corr_result <- read.table(score_result_corr_list[i], header = T, sep = "",
+                                      as.is= T,skip= 1)
+      score_coi <- read.table(score_coi_corr_list[i], header = T, sep = "",
+                              as.is= T,skip= 1)
+      score <- score_corr_result[score_corr_result$ITERATION == -1000000008, "THETA3"]
+      fim  <- score_coi[score_coi$NAME == "THETA3", "THETA3"]
+      score_stat_corr_list[i] <- ifelse(length(fim)>0, score^2/fim, 0)
+    }, error=function(e){cat("ERROR : sim #",conditionMessage(e), "\n")})
+  }
+  final_result_true <- data.frame(sample_size, sample_design, 
+                                  coef=cl_cov_list[coef_index],
+                                  cov_name=cov_corr[1], num_sim, cl_v_corr,
+                                  omegasq, sigmasq,
+                                  lrt=lrt_stat_list,
+                                  wald=wam_stat_list,
+                                  score=score_stat_list)
+  final_result_false <- data.frame(sample_size, sample_design, 
+                                   coef=cl_cov_list[coef_index],
+                                   cov_name=cov_corr[2], num_sim, cl_v_corr,
+                                   omegasq, sigmasq,
+                                   lrt=lrt_stat_corr_list,
+                                   wald=wam_stat_corr_list,
+                                   score=score_stat_corr_list)
+  # create a result directory under the home directory
+  setwd(home_dir)
+  if(!dir.exists("Results")){
+    dir.create("Results")
+  }
+  setwd(paste(home_dir, "Results", sep="/"))
+  write.csv(rbind(final_result_true, final_result_false), 
+            paste(dir_name_0, ".csv", sep=""), row.names = F)  
+}
+
 # the main simulation function
 run_simulation <- function(sample_size, sample_design, coef_index,
-                           cl_cov_list, cov_name, num_sim, omegasq, 
-                           sigmasq, home_dir){
+                           cl_cov_list, cov_name, num_sim, cl_v_corr,
+                           omegasq, sigmasq, home_dir){
   # set the home directory
   setwd(home_dir)
   coef <- cl_cov_list[coef_index]
 
   # create a project directory for each situation
   dir_name_0 <-  paste("Sim", sample_size, sample_design,
-                       cov_name, "omegasq", omegasq, "sigmasq", 
+                       cov_name, "cl_v_corr", cl_v_corr, 
+                       "omegasq", omegasq, "sigmasq", 
                        sigmasq, coef_index, sep = "_")
   if(!dir.exists(dir_name_0)){
     dir.create(dir_name_0)
   }
   proj_dir_0 <- paste(home_dir, "/", dir_name_0, sep = "")
-  files <- c("nmfe74.bat", "pat_sim_data.csv")
+  files <- c("nmfe74.bat", "PAT_SIM_DATA.CSV")
   file.copy(from=files, to=proj_dir_0,
             overwrite = TRUE, recursive = FALSE,
             copy.mode = TRUE)
@@ -310,30 +399,126 @@ run_simulation <- function(sample_size, sample_design, coef_index,
   base_result_list <- paste0("base_model_", 1:num_sim, ".ext")
   cov_model_list <- paste0("cov_model_", 1:num_sim, ".ctl")
   score_model_list <- paste0("score_model_", 1:num_sim, ".ctl")
-  for(i in 1:num_sim){
-    setwd(proj_dir_0)
-    # bootstrap the covariate info from NHANES database
-    create_data_template(sample_design, sample_size, cov_name)
-    # read the median of the covariate
-    cov_median <- read.table("COV_MEDIAN.txt")$V1
-    # create NONMEM script for simulation
-    create_simCovNM(coef, cov_name, cov_median, i, omegasq, sigmasq)
-    # run the sim script
-    run_NM("data_sim.ctl")
-    # clean the sim output from NONMEM
-    sim_df <- read.table(file="DATA_SIM.TAB",
-                         header = T, sep = "",
-                         as.is= T,skip= 1)
-    write.table(sim_df, "DATA_SIM.CSV",  sep=",", row.names = F, col.names = F)
-    
-    # create the base model and run it
-    create_baseNM(base_model_list[i])
-    run_NM(base_model_list[i])
-    # create score test script and run it
-    create_scoreNM(cov_name, cov_median, base_result_list[i], score_model_list[i])
-    run_NM(score_model_list[i])
-    # create  LRT and Wald's test script and run it
-    create_covNM(cov_name, cov_median, cov_model_list[i])
-    run_NM(cov_model_list[i])
+  i <- j <- 1
+  while(i < (1+num_sim)){
+    status <- tryCatch({
+      setwd(proj_dir_0)
+      # bootstrap the covariate info from NHANES database
+      create_data_template(sample_design, sample_size, cov_name)
+      # read the median of the covariate
+      cov_median <- read.table("COV_MEDIAN.TXT")$V1
+      # create NONMEM script for simulation
+      create_simCovNM(coef, cov_name, cov_median, i, 
+                      cl_v_corr, omegasq, sigmasq)
+      # run the sim script
+      run_NM("data_sim.ctl")
+      # clean the sim output from NONMEM
+      sim_df <- read.table(file="DATA_SIM.TAB",
+                           header = T, sep = "",
+                           as.is= T,skip= 1)
+      # delete the negative simulated observation
+      sim_df_1 <- sim_df %>% dplyr::filter(DV>=0) 
+      write.table(sim_df_1, "DATA_SIM.CSV",  sep=",", row.names = F, col.names = F)
+      
+      # create the base model and run it
+      create_baseNM(base_model_list[i])
+      run_NM(base_model_list[i])
+      # create score test script and run it
+      create_scoreNM(cov_name, cov_median, base_result_list[i], score_model_list[i])
+      run_NM(score_model_list[i])
+      # create  LRT and Wald's test script and run it
+      create_covNM(cov_name, cov_median, cov_model_list[i])
+      run_NM(cov_model_list[i])},
+      error = function(e) "error")
+    if (status == "error"){
+      i <- i 
+    } else{
+      i <- i + 1
+    }
+    j <- j + 1
+  }
+}
+
+# the main simulation function for correlated covariate
+# cl_cov_list should not contain 0
+run_simulation_corr <- function(sample_size, sample_design, coef_index,
+                                cl_cov_list, cov_corr, num_sim, cl_v_corr,
+                                omegasq, sigmasq, home_dir){
+  # set the home directory
+  setwd(home_dir)
+  coef <- cl_cov_list[coef_index]
+  cov_true <- cov_corr[1]
+  cov_false <- cov_corr[2]
+  
+  # create a project directory for each situation
+  dir_name_0 <-  paste("Sim", sample_size, sample_design,
+                       paste(cov_corr, collapse = "_"), 
+                       "cl_v_corr", cl_v_corr, 
+                       "omegasq", omegasq, 
+                       "sigmasq", sigmasq, 
+                       coef_index, sep = "_")
+  if(!dir.exists(dir_name_0)){
+    dir.create(dir_name_0)
+  }
+  proj_dir_0 <- paste(home_dir, "/", dir_name_0, sep = "")
+  files <- c("nmfe74.bat", "PAT_SIM_DATA.CSV")
+  file.copy(from=files, to=proj_dir_0,
+            overwrite = TRUE, recursive = FALSE,
+            copy.mode = TRUE)
+  setwd(proj_dir_0)
+  base_model_list <- paste0("base_model_", 1:num_sim, ".ctl")
+  base_result_list <- paste0("base_model_", 1:num_sim, ".ext")
+  cov_model_list <- paste0("cov_model_", 1:num_sim, ".ctl")
+  score_model_list <- paste0("score_model_", 1:num_sim, ".ctl")
+  cov_model_corr_list <- paste0("cov_model_corr", 1:num_sim, ".ctl")
+  score_model_corr_list <- paste0("score_model_corr", 1:num_sim, ".ctl")
+  i <- 1
+  j <- 1
+  while(i < (1+num_sim)){
+    indication <- tryCatch({
+      setwd(proj_dir_0)
+      # bootstrap the covariate info from NHANES database
+      create_data_template(sample_design, sample_size, cov_true)
+      # read the median of the covariate
+      cov_true_median <- read.table("COV_MEDIAN.TXT")$V1
+      # create NONMEM script for simulation
+      create_simCovNM(coef, cov_true, cov_true_median, j, 
+                      cl_v_corr, omegasq, sigmasq)
+      # run the sim script
+      run_NM("data_sim.ctl")
+      # clean the sim output from NONMEM
+      sim_df <- read.table(file="DATA_SIM.TAB",
+                           header = T, sep = "",
+                           as.is= T,skip= 1)
+      # delete the negative simulated observation
+      sim_df_1 <- sim_df %>% dplyr::filter(DV>=0) 
+      write.table(sim_df_1, "DATA_SIM.CSV",  sep=",", row.names = F, col.names = F)
+      # cov_true median
+      cov_false_median <- median(sim_df %>% 
+                                   group_by(ID) %>% 
+                                   filter(row_number() == 1) %>%
+                                   ungroup(ID) %>%
+                                   select(cov_false) %>%
+                                   unlist())
+      # create the base model and run it
+      create_baseNM(base_model_list[i])
+      run_NM(base_model_list[i])
+      # create score test script and run it
+      create_scoreNM(cov_true, cov_true_median, base_result_list[i], score_model_list[i])
+      run_NM(score_model_list[i])
+      create_scoreNM(cov_false, cov_false_median, base_result_list[i], score_model_corr_list[i])
+      run_NM(score_model_corr_list[i])
+      # create  LRT and Wald's test script and run it
+      create_covNM(cov_true, cov_true_median, cov_model_list[i])
+      run_NM(cov_model_list[i])
+      create_covNM(cov_false, cov_false_median, cov_model_corr_list[i])
+      run_NM(cov_model_corr_list[i])},
+      error = function(e) "error")
+    if (indication == "error"){
+      i <- i 
+    } else{
+      i <- i + 1
+    }
+    j <- j + 1
   }
 }
--- simulation.R
+++ simulation.R
@@ -3,7 +3,7 @@
 #        for Covariate Testing in PopPK
 # Author: Yixuan Zou
 # Create Date: 5/26/2020
-# Last Update: 6/28/2020
+# Last Update: 7/12/2020
 # CAUTION: You need to put NONMEM batch file into the folder
 #-------------------------------------------------------------
 
@@ -11,19 +11,12 @@
 #### r setting
 # clear all objects
 rm(list=ls(all=TRUE))
-
-# load packages
-requiredPackages <- c("ggplot2", "dplyr", "tidyr", "ggthemes")
-for(p in requiredPackages){
-  if(!require(p,character.only = TRUE)) install.packages(p)
-  library(p,character.only = TRUE)
-}
 #-------------------------------------------------------------
 
 #-------------------------------------------------------------
 #### simulation setting (change here)
 # number of simulation
-num_sim <- 10
+num_sim <- 1000
 
 # home directory
 home_dir <- "D:/Pharmacy/Projects/Cov_test"
@@ -32,40 +25,51 @@ source("functions.r")
 
 # sample size
 sample_size_list <- c(50, 100, 200)
-
 # coefficient of effects of continuous variable on clearance
 # when coefficient=0 power is calculated
 cl_cont_list <- c(0, 0.1, 0.25, 0.5, 0.75, 1)
-
 # coefficient of effects of categorical variable on clearance
 cl_cat_list <- c(0, 0.1, 0.25, 0.5, 0.75, 1)
-
 # sample desgin
 sample_design_list <- c('Sparse', 'Intensive')
-
 # omega sqaure
 omegasq_list <- c(0.03, 0.1)
-
 # sigma sqaure
 sigmasq_list <- c(0.03, 0.1)
+# clearance and volume correlation
+# in our dicussion the correlation can be 0, 0.3
+cl_v_corr_list <- c(0)
+# correlated covariate
+cov_corr_list <- list(c("WT","BSA"),
+                      c("WT", "HT"),
+                      c("WT", "ALP"))
+num_sim <- 10
+sample_size_list <- 50
+cl_cont_list <- 1
+cl_cat_list <- 1
+sample_design_list <- "Sparse"
+omegasq_list <- 0.1
+sigmasq_list <- 0.1
+cov_corr_list <- list(c("WT","HT"))
 #-------------------------------------------------------------
-# print all the covariate names
-demo_df <- read.csv("pat_sim_data.csv")
-print(colnames(demo_df))
 
-# run simulation for each situation
+#-------------------------------------------------------------
+#### Part 1: single covariate
+# run simulation for each scenario
 start_time <- Sys.time()
 for(sample_size in sample_size_list){
   for(sample_design in sample_design_list){
     for(index in 1:length(cl_cat_list)){
       for(omegasq in omegasq_list){
         for(sigmasq in sigmasq_list){
-          run_simulation(sample_size, sample_design, index,
-                         cl_cont_list, "WT", num_sim,
-                         omegasq, sigmasq, home_dir)
-          run_simulation(sample_size, sample_design, index,
-                         cl_cat_list, "SEX", num_sim,
-                         omegasq, sigmasq, home_dir)
+          for(cl_v_corr in cl_v_corr_list){
+            run_simulation(sample_size, sample_design, index,
+                           cl_cont_list, "WT", num_sim, cl_v_corr,
+                           omegasq, sigmasq, home_dir)
+            run_simulation(sample_size, sample_design, index,
+                           cl_cat_list, "SEX", num_sim, cl_v_corr,
+                           omegasq, sigmasq, home_dir)
+          }
         }
       }
     }
@@ -75,40 +79,69 @@ run_time <- format(Sys.time() - start_time)
 setwd(home_dir)
 write.table(run_time, file = "run_time.txt", sep = "", 
             row.names = F, col.names = F)
-#-------------------------------------------------------------
+
 # analyze the results
 for(sample_size in sample_size_list){
   for(sample_design in sample_design_list){
     for(index in 1:length(cl_cat_list)){
       for(omegasq in omegasq_list){
         for(sigmasq in sigmasq_list){
-          run_analysis(sample_size, sample_design, index,
-                       cl_cont_list, "WT", num_sim,
-                       omegasq, sigmasq, home_dir)
-          run_analysis(sample_size, sample_design, index,
-                       cl_cat_list, "SEX", num_sim,
-                       omegasq, sigmasq, home_dir)
+          for (cl_v_corr in cl_v_corr_list) {
+            run_analysis(sample_size, sample_design, index,
+                         cl_cont_list, "WT", num_sim, cl_v_corr,
+                         omegasq, sigmasq, home_dir)
+            run_analysis(sample_size, sample_design, index,
+                         cl_cat_list, "SEX", num_sim, cl_v_corr,
+                         omegasq, sigmasq, home_dir) 
+          }
         }
       }
     }
   }
 }
+
 #-------------------------------------------------------------
-# pilot simulation 
-# sample_size <- 100
-# sample_design <- 'Intensive'
-# i <- 0
-# num_sim <- 1000
-# cov_name <- "WT"
-# omegasq <- sigmasq <- 0.1
-# start_time <- Sys.time()
-# run_simulation(sample_size, sample_design, i,
-#                cl_cont_list, "WT", num_sim,
-#                omegasq, sigmasq, home_dir)
-# run_time <- format(Sys.time() - start_time)
-# setwd(home_dir)
-# write.table(run_time, file = "run_time.txt", sep = "", row.names = F, col.names = F)
-# 
-# run_analysis(sample_size, sample_design, i,
-#              cl_cont_list, "WT", num_sim,
-#              omegasq, sigmasq, home_dir)
+
+#-------------------------------------------------------------
+#### Part 2: correlated covariate
+start_time <- Sys.time()
+for(sample_size in sample_size_list){
+  for(sample_design in sample_design_list){
+    for(index in 1:length(cl_cat_list)){
+      for(omegasq in omegasq_list){
+        for(sigmasq in sigmasq_list){
+          for(cl_v_corr in cl_v_corr_list){
+            for(cov_corr in cov_corr_list){
+              run_simulation_corr(sample_size, sample_design, index,
+                                  cl_cont_list, cov_corr, num_sim, 
+                                  cl_v_corr, omegasq, sigmasq, home_dir)
+            }
+          }
+        }
+      }
+    }
+  }
+}
+run_time <- format(Sys.time() - start_time)
+setwd(home_dir)
+write.table(run_time, file = "run_time_corr.txt", sep = "", 
+            row.names = F, col.names = F)
+
+# analyze the results
+for(sample_size in sample_size_list){
+  for(sample_design in sample_design_list){
+    for(index in 1:length(cl_cat_list)){
+      for(omegasq in omegasq_list){
+        for(sigmasq in sigmasq_list){
+          for(cl_v_corr in cl_v_corr_list){
+            for(cov_corr in cov_corr_list){
+              run_analysis_corr(sample_size, sample_design, index,
+                                cl_cont_list, cov_corr, num_sim, 
+                                cl_v_corr, omegasq, sigmasq, home_dir)
+            }
+          }
+        }
+      }
+    }
+  }
+}
